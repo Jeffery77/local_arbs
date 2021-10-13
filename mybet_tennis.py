e4358581d7cb3c0632db9bf19da304d9eca1e976
Jeffery77 <33126237+Jeffery77@users.noreply.github.com>
2021-10-13 22:10:07 +0000

    update on mybet

--- mybet_tennis.py
+++ mybet_tennis.py
@@ -14,12 +14,37 @@ import json
 import csv
 
 
+def get_info():
+    tournament_xpath = '//h1[@class="tournament-header--title"]'
+    Date_xpath = '//*[@id="nvsOddsPage"]/div/div[1]/div[4]/div/nvs-odds-table/div/div[2]/div[1]/div[1]/h3/nvs-configure-date-time-for-view'
+    Date_xpath_format = '//*[@id="nvsOddsPage"]/div/div[{}]/div[4]/div/nvs-odds-table/div/div[{}]/div[1]/div[1]/h3/nvs-configure-date-time-for-view'
+
+    tournament_header = wait.until(EC.presence_of_all_elements_located((By.XPATH,tournament_xpath)))
+    Date = wait.until(EC.presence_of_element_located((By.XPATH,Date_xpath)))
+    length = len(tournament_header)
+
+    for i in range(length):
+        if i == 0:
+            continue
+        Date_xpath_format = Date_xpath_format.format(i,2)
+        try:
+            Date = wait.until(EC.presence_of_element_located((By.XPATH,Date_xpath_format)))
+            Date_list.append(Date.text)
+        except TimeoutException:
+            pass
+    
+    for t in tournament_header:
+        print(t.text)
+
+
+        print(Date_list)
+
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
 
 driver = webdriver.Chrome(executable_path='chromedriver.exe',options=options)
 wait = WebDriverWait(driver,20)
-
+Date_list = []
 
 driver.get('https://www.mybet.africa/sport/odds?timeFrame=all')
 
@@ -31,5 +56,8 @@ driver.execute_script("arguments[0].click();", tennis_tab)
 icon_select = wait.until(EC.presence_of_element_located((By.XPATH,icon_select_xpath)))
 
 driver.execute_script("arguments[0].click();",icon_select)
+get_info()
+
+
 
 
