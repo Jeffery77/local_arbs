282d63566f28bfd507dad09f9a70366ecfce57df
Jeffery77 <33126237+Jeffery77@users.noreply.github.com>
2021-10-25 21:16:19 +0000

    mybet tennis done

--- betika.py
+++ betika.py
@@ -5,3 +5,35 @@ Created on Thu Sep  2 10:29:13 2021
 @author: dell
 """
 
+from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException,TimeoutException   
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import json
+import csv
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+
+driver = webdriver.Chrome(executable_path='chromedriver.exe',options=options)
+
+
+wait = WebDriverWait(driver,20)
+driver.get('https://betika.com.gh/s/tennis')
+upcoming_xpath = '/html/body/div[3]/main/div[1]/div/div[2]/div/div[2]/button'
+upcoming_xpath1 = '//button[@class="button account__payments__submit button button__secondary matches__more no-bg"]'
+
+#load_matches = "/html/body/div[3]/main/div[1]/div/div[2]/div/div[2]/button"
+
+
+upcoming_button = wait.until(EC.presence_of_element_located((By.XPATH,upcoming_xpath1)))
+
+driver.execute_script("arguments[0].click();",upcoming_button)
+driver.execute_script("arguments[0].click();",upcoming_button)
+tournament_xpath = '//div[@class="pull-left"]'
+
+tournaments = wait.until(EC.presence_of_all_elements_located((By.XPATH,tournament_xpath)))
+for t in tournaments:
+    print(t.text)
--- mybet_tennis.py
+++ mybet_tennis.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Sep 29 21:09:20 2021
 
-@author: dell
+@author: dell 
 """
 
 from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException,TimeoutException   
@@ -14,6 +14,45 @@ import json
 import csv
 import time
 
+def write_to_csv(y):
+    filename = "sorted_mybet_tennis.csv"
+    with open(filename,'w') as csvfile:
+    # creating a csv writer object
+        csvwriter = csv.writer(csvfile)
+        fields = ['Team1','Team2','odd1','odd2','date','league']
+    
+      # writing the fields
+        csvwriter.writerow(fields)
+    
+     # writing the data rows
+    
+        csvwriter.writerows(y)
+        print("csv File created")
+        
+def sort_info(num,g,date,league):
+    gt = (g[num].split("\n"))
+
+    count = 0
+    team_info = []
+    large_team_info = []
+    for i, t in enumerate(gt):
+        #print(i)
+    
+        if ":" in t and i != 0:
+            #print(team_info)
+            team = team_info[1].split("-")
+            da = team_info[0] + " " + date
+            team_info_a = [team[0],team[1],team_info[2],team_info[3],da,league]
+            large_team_info.append(team_info_a)
+            team_info = []
+        team_info.append(t)
+    team = team_info[1].split("-")
+    da = team_info[0] + " " + date
+    team_info_a = [team[0],team[1],team_info[2],team_info[3],da,league]
+    large_team_info.append(team_info_a)
+    for l in large_team_info:
+        final_list.append(l)
+    print(large_team_info)
 
 def get_info():
     tournament_xpath = '//*[@id="nvsOddsPage"]/div/div/div[1]/div[2]/h1/span'
@@ -32,35 +71,16 @@ def get_info():
         #print(t.text)
     print(tournament_header.text)
     print(Date.text)
-    print(team_list)
+    #print(team_list)
+    sort_info(0,team_list,Date.text,tournament_header.text)
     print
     try:
         Date_2 = wait2.until(EC.presence_of_element_located((By.XPATH,Date_xpath_2)))
         print(Date_2.text)
+        sort_info(1,team_list,Date.text,tournament_header.text)
     except TimeoutException:
         pass
         
-    #Date_list = []
-    #tournament_list = []
-    #for i in range(length):
-    #    if i == 0:
-    #        continue
-    #    Date_xpath_format = '//*[@id="nvsOddsPage"]/div/div[{}]/div[4]/div/nvs-odds-table/div/div[{}]/div[1]/div[1]/h3/nvs-configure-date-time-for-view'
-    #    Date_xpath_format = Date_xpath_format.format(i,2)
-      
-    #    try:
-    #        Date = wait.until(EC.presence_of_element_located((By.XPATH,Date_xpath_format)))
-    #        Date_list.append(Date.text)
-    #    except TimeoutException:
-    #        pass
-    
-    #for t in tournament_header:
-    
-    #    tournament_list.append(t.text)
-
-
-    #print(Date_list)
-    #print(tournament_list)
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
@@ -69,10 +89,15 @@ driver = webdriver.Chrome(executable_path='chromedriver.exe',options=options)
 wait = WebDriverWait(driver,20)
 wait2 = WebDriverWait(driver,5)
 Date_list = []
+final_list = []
 
 driver.get('https://www.mybet.africa/sport/odds?timeFrame=all')
+# Change the locator when the tennis tab has been shifted
+tennis_locator_number = 4
 
-tennis_tab_xpath = '//*[@id="nvs-left-sidebar"]/div/div[5]/nvs-widget-list-manager/div/div[2]/div[4]/div[1]/span[2]'
+  #                 //*[@id="nvs-left-sidebar"]/div/div[5]/nvs-widget-list-manager/div/div[2]/div[4]/div[1]
+tennis_tab_xpath = '//*[@id="nvs-left-sidebar"]/div/div[5]/nvs-widget-list-manager/div/div[2]/div[{}]/div[1]/span[2]'
+tennis_tab_xpath = tennis_tab_xpath.format(tennis_locator_number)
 tennis_tab = wait.until(EC.presence_of_element_located((By.XPATH,tennis_tab_xpath)))
 
 driver.execute_script("arguments[0].click();", tennis_tab)
@@ -81,8 +106,6 @@ for v in range(15):
     flag = True
     if v == 0:
         continue
-   
-
    
     tournament_dropdown_xpath = '//*[@id="nvs-left-sidebar"]/div/div[5]/nvs-widget-list-manager/div/div[2]/div[4]/div[2]/div[{}]/div[1]/div[3]/i'
 
@@ -97,8 +120,9 @@ for v in range(15):
     p = 1
     while flag:
         try:
-            icon_select_xpath = '//*[@id="nvs-left-sidebar"]/div/div[5]/nvs-widget-list-manager/div/div[2]/div[4]/div[2]/div[{}]/div[2]/label[{}]/span[1]'
-            icon_select_xpath = icon_select_xpath.format(v,p)
+       
+            icon_select_xpath = '//*[@id="nvs-left-sidebar"]/div/div[5]/nvs-widget-list-manager/div/div[2]/div[{}]/div[2]/div[{}]/div[2]/label[{}]/span[1]'
+            icon_select_xpath = icon_select_xpath.format(tennis_locator_number,v,p)
             #.000021415869++-print(icon_select_xpath)
             icon_select = wait.until(EC.presence_of_element_located((By.XPATH,icon_select_xpath)))
             time.sleep(2)
@@ -112,7 +136,9 @@ for v in range(15):
             
     #get_info()
     print("next league title")
-    
+#print("Final list:")
+#print(final_list)  
+write_to_csv(final_list)
     #driver.execute_script("arguments[0].click();",tournament_dropdown)
 
 
